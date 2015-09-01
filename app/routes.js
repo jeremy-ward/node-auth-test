6c916940ce9617e8f47db2502fc9101146ae911c
Jeremy Ward <jerskiward@gmail.com>
2015-09-01 21:40:55 +0000

    fix bugs in route.js file

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ module.exports=function(app,passport){
     
     //process the login form
     app.post("/login", passport.authenticate('local-login',{
-        succesRedirect:'/profile', //redirect to the secure profile section
+        successRedirect:'/profile', //redirect to the secure profile section
         failureRedirect:'/login', //redirect to the login page if there is an error
         failureFlash: true  //allow flash messages
     }));
@@ -26,7 +26,7 @@ module.exports=function(app,passport){
     
     //process the sign-up form
     app.post("/signup",passport.authenticate('local-signup',{
-        succesRedirect:'/profile', //redirect to the secure profile section
+        successRedirect:'/profile', //redirect to the secure profile section
         failureRedirect:'/signup', //redirect to the signup page if there is an error
         failureFlash: true  //allow flash messages
     }));
